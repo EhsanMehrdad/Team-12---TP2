9d86e2b82e75d2a494f33bf5b9ba2becc97fd968
KSGreene <210071946.aston.ac.uk>
2023-03-01 23:21:39 +0000

    updated

--- indiproduct/connected.php
+++ indiproduct/connected.php
@@ -1,25 +1,13 @@
 <?php
-  /*  $dbname     = "u_210071946_db";
+    $dbname     = "cs2tp";
     $servername = "localhost";
-    $username   = "u-210071946";
-    $password   = "rQKLO9BvbfqmuK0";
+    $username   = "root";
+    $password   = "";
     try {
         $db = new PDO("mysql:dbname=$dbname;host=$servername;", $username, $password);
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        echo "helll";
     } catch(PDOException $e) {
         echo $e->getMessage();
         exit;
-    }*/
-
-try{
-//$db=new PDO("mysql:dbname=u_210071946_db;host=localhost", "u-210071946","rQKLO9BvbfqmuK0");
-$db=new PDO("mysql:dbname=astoncv;host=localhost", "root","");
-
-} catch (PDOException $ex) { 
-    ?>
-    <p>Sorry,  a database error occurred.</p>
-    <p> Error details: <em> <?= $ex->getMessage() ?> </em></p>
-    <?php
     }
 ?>
