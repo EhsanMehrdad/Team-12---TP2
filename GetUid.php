6ec94a6ae02e8ce417f25deac8e1b1773d566191
Razik ali <101721912+RazikAli@users.noreply.github.com>
2023-03-16 15:27:08 +0000

    Delete GetUid.php

--- GetUid.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-$db = mysqli_connect("localhost","root","","u_200219998_db");
-    if (mysqli_connect_errno()){
-	echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	die();
-	}
-?>
