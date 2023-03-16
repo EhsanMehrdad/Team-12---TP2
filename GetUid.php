71021148c82f330def22d2a33723369fd033f866
Razik ali <101721912+RazikAli@users.noreply.github.com>
2023-03-16 15:52:05 +0000

    changes
    chnages

--- GetUid.php
+++ GetUid.php
@@ -1,6 +1,6 @@
 
 <?php
-$db = mysqli_connect("localhost","root","","u_200219998_db");
+$db = mysqli_connect("localhost","root","","u_200219998_db"); 
     if (mysqli_connect_errno()){
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 	die();
